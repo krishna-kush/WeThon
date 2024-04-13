69b192377c63db88cbd753512b8991bd60973728
Shivam Kumar <skumarshivam50@gmail.com>
2024-04-13 08:49:00 +0530

    ek aur gaand fad commit

--- frontend/src/routes/allthons/components/Card.jsx
+++ frontend/src/routes/allthons/components/Card.jsx
@@ -2,55 +2,55 @@ import React from 'react'
 
 
 function Card() {
-  return (
-    <div className='w-full'>
+    return (
+        <div className='w-full'>
 
-    <div className='border rounded-xl rounded-r-none border-green-300 bg-[#302f2f]  h-fit w-full grid grid-cols-3 shadow-lg dark:bg-[#3B3E47] dark:hover:bg-[#53565e] hover:bg-[#00000020]  hover:cursor-pointer transform hover:scale-110 transition-transform duration-300'>
-        <div className='  h-full  col-span-2 p-6 flex ' >
-<div 
-    className='h-28 min-w-28 bg-white bg-center bg-cover rounded-lg'
-    style={{
-        backgroundImage: "url('https://d112y698adiu2z.cloudfront.net/photos/production/challenge_thumbnails/002/812/484/datas/medium_square.png')"
-    }}
-></div>
-            <div className='pl-4 flex gap-2 flex-col w-full'>   
-                <h1 className='text-white text-xl font-bold'>Thon Name</h1>
-                <div className='flex justify-between w-full text-sm text-gray-300 pt-2'>
-                    <div> 
-                        <span className=' bg-green-500 px-2 rounded-2xl py-0.5 text-white'>20 days left</span>
-                        <div className='pt-2'>Mar 18 - May 03, 2024</div>
+            <div className='border rounded-xl rounded-r-none border-green-300 bg-[#302f2f]  h-fit w-full grid grid-cols-3 shadow-lg dark:bg-[#3B3E47] dark:hover:bg-[#53565e] hover:bg-[#00000020]  hover:cursor-pointer transform hover:scale-110 transition-transform duration-300'>
+                <div className='  h-full  col-span-2 p-6 flex ' >
+                    <div
+                        className='h-28 min-w-28 bg-white bg-center bg-cover rounded-lg'
+                        style={{
+                            backgroundImage: "url('https://d112y698adiu2z.cloudfront.net/photos/production/challenge_thumbnails/002/812/484/datas/medium_square.png')"
+                        }}
+                    ></div>
+                    <div className='pl-4 flex gap-2 flex-col w-full'>
+                        <h1 className='text-white text-xl font-bold'>Thon Name</h1>
+                        <div className='flex justify-between w-full text-sm text-gray-300 pt-2'>
+                            <div>
+                                <span className=' bg-green-500 px-2 rounded-2xl py-0.5 text-white'>20 days left</span>
+                                <div className='pt-2'>Mar 18 - May 03, 2024</div>
+                            </div>
+                            <div>
+                                <div className='mb-2'><b>50,000</b> in prizes</div>
+                                <div><b>11000 </b>particepents</div>
+                            </div>
+                        </div>
+
+                        <div className='text-white pt-2'>
+                            this will be the desctiption of the thon
+                        </div>
+
                     </div>
-                    <div> 
-                        <div className='mb-2'><b>50,000</b> in prizes</div>
-                        <div><b>11000 </b>particepents</div>
+
+                </div>
+                <div className='border-l border-l-green-300/30  border-r-8 border-green-300  h-full p-8'>
+                    <a href="www.google.com"> <Topics topics={['Google']}></Topics></a>
+                    <div className='pt-2 text-white'>Mar 18 - May 03, 2024</div>
+                    <div className='flex flex-col gap-2 pt-2 bg-blue-100/0 rounded-lg py-2 px-1 mt-2'>
+                        <Topics topics={['Machine Learning', 'Data Science']} />
                     </div>
                 </div>
-
-                <div className='text-white pt-2'>
-                    this will be the desctiption of the thon
-                </div>
-                
-            </div>
-
-        </div>
-        <div className='border-l border-l-green-300/30  border-r-8 border-green-300  h-full p-8'>
-            <a href="www.google.com"> <Topics topics={['Google']}></Topics></a>
-            <div className='pt-2 text-white'>Mar 18 - May 03, 2024</div>
-            <div className='flex flex-col gap-2 pt-2 bg-blue-100/0 rounded-lg py-2 px-1 mt-2'>
-                <Topics topics={['Machine Learning','Data Science']}/>
             </div>
         </div>
-    </div>
-    </div>
-  )
+    )
 }
 export default Card
 
 
-function Topics({topics}){
+function Topics({ topics }) {
     return (
         <>
-            {topics.map(topic=>(
+            {topics.map(topic => (
                 <span className='border bg-blue-800/10 border-blue-500 text-blue-500 px-2 rounded-2xl py-0.5 w-fit '>{topic}</span>
             ))}
         </>
--- frontend/src/routes/createthon/Createthon.jsx
+++ frontend/src/routes/createthon/Createthon.jsx
@@ -1,9 +1,77 @@
+import { Input } from '@mui/material'
+import { Datepicker, FileInput } from 'flowbite-react'
 import React from 'react'
 
+
 function Createthon() {
-  return (
-    <div>Createthon</div>
-  )
+    return (
+        <div className=' bg-[#232323] max-h-full min-h-screen pb-24 flex flex-col justify-center items-center gap-8'>
+            <div className='flex items-center flex-col  '>
+                <div className=' p-4 text-white text-[50px] font-bold'>Create HackaThons</div>
+                <div className='max-w-[800px] text-center p-4 text-white/60  text-2xl '>Fill the details to create hackathon on <b>weThon.</b></div>
+            </div>
+
+            <div className='w-full flex justify-center'>
+                <div className=' w-3/6 flex flex-col gap-8'>
+                    <div className='text-white flex flex-col gap-2'>
+                        <div className='flex items-center gap-8 pb-4 justify-between'>
+                            <div
+                                className='h-28 w-28 bg-white bg-center bg-cover rounded-full'
+                                style={{
+                                    backgroundImage: "url('https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png')"
+                                }}
+                            ></div>
+                            <div className='flex flex-col gap-4'>
+                                <div>
+                                    Profile<FileInput className='border-none' />
+                                </div>
+                                <div>
+                                    Banner<FileInput className='border-none' />
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+
+                            <div className='text-white text-lg'>Hackathon Name</div>
+                            <input className='w-full p-2 bg-[#302f2f] rounded-lg' type="text" />
+                        </div>
+                        <div>
+                            <div className='text-white text-lg pt-4'>Hackathon Description</div>
+                            <textarea className='w-full p-2 h-28 bg-[#302f2f] rounded-lg' type="text" />
+                        </div>
+                        <div className='flex items-center justify-between '>
+                            <div>
+                                <div className='textx-white text-lg pt-4'>Start Date</div>
+                                <Datepicker />
+                            </div>
+                            <div className='w-1/2 flex flex-col justify-end items-end'>
+                                <div className='text-white text-lg pt-4'>End Date</div>
+                                <Datepicker />
+                            </div>
+                        </div>
+                        <div className='flex justify-between '>
+                            <div className='w-1/2'>
+                                <div className='text-white text-lg pt-4'>Prizepool  ( In INR )</div>
+                                <input className=' p-2 bg-[#302f2f] rounded-lg w-2/3' placeholder='99999' type="number" />
+                            </div>
+                            <div className='w-1/2 flex flex-col justify-end items-end'>
+                                <div className='text-white text-lg pt-4 '>Registration Fee  ( In INR )</div>
+                                <input className='w-2/3 p-2 bg-[#302f2f] rounded-lg' placeholder='99999' type="number" />
+                            </div>
+
+                        </div>
+                        <div>
+                            <div className='text-white text-lg pt-4'>Topics</div>
+                            <input className='w-full p-2 bg-[#302f2f] rounded-lg' type="text" />
+                        </div>
+                        <div className='text-white text-lg pt-4'>Rules</div>
+                        <textarea className='w-full p-2 h-28 bg-[#302f2f] rounded-lg' type="text" />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Createthon
