03989432f0215d6d2a43cbd1ccca115b04134215
PranavKumar9529desai <dpranav7745@gmail.com>
2024-04-13 13:26:28 +0530

    my single commit = your all commits so don't mess with me.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './routes/Home'
+import Home2 from './routes/home/Home'
 import Signin from './routes/signin/Signin'
 import Signup from './routes/signup/Signup'
 import Hack from './routes/Hack/index'
@@ -21,7 +21,9 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          {/* <Route path="/home*" element={<span />} /> */}
           <Route path="/*" element={<Header />} />
+
         </Routes>
 
         <Routes>
@@ -32,10 +34,11 @@ function App() {
           <Route path='/hack' element={<Hack/>} />
           <Route path="/allthons" element={<AllThons />} />
           <Route path="/header" element={<Header />} />
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
           <Route path='/createthon' element={<Createthon/>} />
           <Route path='/managethon' element={<Managethon/>} />
           {/* <Route path="/codespace" element={<CodeSpace />} /> */}
+          <Route path='/home' element={<Home2/>}/>
 
 
           <Route path='/*' element={<PageNotFound/>} />
