533c97e704271b13b5f60790c8303024bb68c550
Shivam Kumar <skumarshivam50@gmail.com>
2024-04-13 12:54:11 +0530

    major commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import AllThons from './routes/allthons/AllThons'
 import CodeSpace from './routes/Hack/codespace'
 import Createthon from './routes/createthon/Createthon'
 import Managethon from './routes/managethon/Managethon'
+import SingleHack from './routes/singlehack/SingleHack'
 
 import PageNotFound from './routes/PageNotFound'
 
@@ -36,6 +37,8 @@ function App() {
           <Route path='/createthon' element={<Createthon/>} />
           <Route path='/managethon' element={<Managethon/>} />
           {/* <Route path="/codespace" element={<CodeSpace />} /> */}
+          <Route path='/hack_description' element={<SingleHack/>} />
+          {/* <Route path='/*' element={<PageNotFound/>} /> */}
 
 
           <Route path='/*' element={<PageNotFound/>} />
