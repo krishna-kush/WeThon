66d07ceb76679256284be01b3a4ebcdee9409fde
Shivam Kumar <skumarshivam50@gmail.com>
2024-04-13 07:59:06 +0530

    allthons improved

--- frontend/src/routes/allthons/AllThons.jsx
+++ frontend/src/routes/allthons/AllThons.jsx
@@ -8,12 +8,16 @@ function AllThons() {
   return (
     <div className=' bg-[#232323] max-h-full min-h-screen pb-24 flex flex-col justify-center items-center gap-8'>
       <div className='flex items-center flex-col '> 
-        <div className=' p-4 text-white text-[50px] font-bold'>All Thons</div>
-        <div className=' p-4 text-white/60  text-2xl'>Find all the thons here</div>
+        <div className=' p-4 text-white text-[50px] font-bold'>Discover HackaThons</div>
+        <div className='max-w-[800px] text-center p-4 text-white/60  text-2xl'>Dicover all the hackhathons happening in your sorrounding and get chance to compete with others. </div>
       </div>
       <Filter/>
       <div className='w-full flex justify-center'>
         <div className=' w-3/6 flex flex-col gap-8'> 
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
           <Card/>
           <Card/>
           <Card/>
--- frontend/src/routes/allthons/components/Card.jsx
+++ frontend/src/routes/allthons/components/Card.jsx
@@ -5,10 +5,15 @@ function Card() {
   return (
     <div className='w-full'>
 
-    <div className='border rounded-xl rounded-r-none border-green-300 bg-[#302f2f]  h-fit w-full grid grid-cols-3 shadow-lg dark:bg-[#3B3E47] dark:hover:bg-[#53565e] hover:bg-[#00000020] duration-300 '>
+    <div className='border rounded-xl rounded-r-none border-green-300 bg-[#302f2f]  h-fit w-full grid grid-cols-3 shadow-lg dark:bg-[#3B3E47] dark:hover:bg-[#53565e] hover:bg-[#00000020]  hover:cursor-pointer transform hover:scale-110 transition-transform duration-300'>
         <div className='  h-full  col-span-2 p-6 flex ' >
-            <div className='h-28 min-w-28 bg-white'></div>
-            <div className='pl-4 flex gap-2 flex-col w-full'>
+<div 
+    className='h-28 min-w-28 bg-white bg-center bg-cover rounded-lg'
+    style={{
+        backgroundImage: "url('https://d112y698adiu2z.cloudfront.net/photos/production/challenge_thumbnails/002/812/484/datas/medium_square.png')"
+    }}
+></div>
+            <div className='pl-4 flex gap-2 flex-col w-full'>   
                 <h1 className='text-white text-xl font-bold'>Thon Name</h1>
                 <div className='flex justify-between w-full text-sm text-gray-300 pt-2'>
                     <div> 
@@ -28,7 +33,7 @@ function Card() {
             </div>
 
         </div>
-        <div className='border-l border-r-8 border-green-300  h-full p-8'>
+        <div className='border-l border-l-green-300/30  border-r-8 border-green-300  h-full p-8'>
             <a href="www.google.com"> <Topics topics={['Google']}></Topics></a>
             <div className='pt-2 text-white'>Mar 18 - May 03, 2024</div>
             <div className='flex flex-col gap-2 pt-2 bg-blue-100/0 rounded-lg py-2 px-1 mt-2'>
@@ -46,7 +51,7 @@ function Topics({topics}){
     return (
         <>
             {topics.map(topic=>(
-                <span className='border bg-blue-200 border-blue-500 text-blue-500 px-2 rounded-2xl py-0.5 w-fit '>{topic}</span>
+                <span className='border bg-blue-800/10 border-blue-500 text-blue-500 px-2 rounded-2xl py-0.5 w-fit '>{topic}</span>
             ))}
         </>
     )
